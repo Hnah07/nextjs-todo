d70768db814a0b886712eb72e8383dced1a961d7
Hannah Casier <hannah.casier@me.com>
2025-05-26 15:40:35 +0200

    updated padding on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>
-        <main className="flex flex-col items-center h-screen py-10 max-w-xl mx-auto gap-4">
+        <main className="flex flex-col items-center h-screen py-10 max-w-xl mx-auto gap-4 px-4 mobile:px-6">
           {children}
         </main>
         <Toaster />
