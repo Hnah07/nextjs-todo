f458bda5c6eba63ea6666208268a2b564c17c842
Hannah Casier <hannah.casier@me.com>
2025-06-02 11:22:31 +0200

    en opnieuw

--- src/app/api/sheet/route.js
+++ src/app/api/sheet/route.js
@@ -27,7 +27,7 @@ async function getSheetClient() {
     }
     credentials = {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
     };
   } else {
     // In development with JSON file available, use it
